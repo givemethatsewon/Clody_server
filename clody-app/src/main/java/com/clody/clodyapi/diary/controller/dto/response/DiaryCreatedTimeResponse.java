cf66ec0c8df521e65b8fbe5c68786834b6b3d385
강현욱 <43662405+hyunw9@users.noreply.github.com>
2025-03-04 20:54:16 +0900

    Feat : 시간 조회 시 날짜 응답 로직 구현  (#210)
    * [Feat] 시간 조회 시, 날짜도 반환하도록 수정
    
    * [Feat] : api 모듈 dto에도 변경사항 반영

--- clody-app/src/main/java/com/clody/clodyapi/diary/controller/dto/response/DiaryCreatedTimeResponse.java
+++ clody-app/src/main/java/com/clody/clodyapi/diary/controller/dto/response/DiaryCreatedTimeResponse.java
@@ -1,12 +1,15 @@
 package com.clody.clodyapi.diary.controller.dto.response;
 
+import java.time.LocalDate;
+
 public record DiaryCreatedTimeResponse(
     int HH,
     int mm,
     int ss,
+    LocalDate date,
     boolean isFirst
 ) {
-    public static DiaryCreatedTimeResponse of(int HH, int mm, int ss, boolean isFirst) {
-        return new DiaryCreatedTimeResponse(HH, mm, ss, isFirst);
+    public static DiaryCreatedTimeResponse of(int HH, int mm, int ss,LocalDate date, boolean isFirst) {
+        return new DiaryCreatedTimeResponse(HH, mm, ss, date, isFirst);
     }
 }
