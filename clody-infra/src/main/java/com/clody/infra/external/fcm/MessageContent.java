e830a92fca5e87077a0a5c34e8ce4b35d6c6fd0b
강현욱 <43662405+hyunw9@users.noreply.github.com>
2025-03-30 20:43:24 +0900

    feat : 푸시 알림 메세지 문구 업데이트 (#222)
    유니코드는 정상적으로 모바일에서 보여집니다.

--- clody-infra/src/main/java/com/clody/infra/external/fcm/MessageContent.java
+++ clody-infra/src/main/java/com/clody/infra/external/fcm/MessageContent.java
@@ -7,8 +7,8 @@ import lombok.Getter;
 @Getter
 public enum MessageContent {
 
-  DIARY_WRITE_REQUEST("클로디","오늘의 감사한 일을 말해보세요!"),
-  REPLY_COMPLETED_REQUEST("클로디","행운의 답장이 도착했어요!"),
+  DIARY_WRITE_REQUEST("클로디","하루를 돌아보며 감사했던 순간을 적어 보세요 \uD83D\uDE4F"),
+  REPLY_COMPLETED_REQUEST("클로디","로디가 전하는 행운의 답장이 도착했어요 \uD83D\uDC8C"),
   ;
   public final String title;
   public final String body;
